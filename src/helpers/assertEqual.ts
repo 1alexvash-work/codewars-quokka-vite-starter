5fea18ac698b0c4bf8bf4358fcead004b7cf99ae
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-03-27 11:35:45 +0200

    Slightly improved the error message

--- src/helpers/assertEqual.ts
+++ src/helpers/assertEqual.ts
@@ -7,6 +7,6 @@ export const assertEqual = ({ actual, expected }: AssertEqual) => {
   if (JSON.stringify(actual) === JSON.stringify(expected)) {
     return `✅ ${actual} === ${expected}`;
   } else {
-    return `❌ ${actual} !== ${expected}`;
+    return `❌ Failed: Expected ${expected}, but got ${actual}`;
   }
 };
