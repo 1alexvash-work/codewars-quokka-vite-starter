a9b6fd2109efdd285775a983ce61d495d1eb68d7
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-05-22 19:40:40 +0300

    temp stage commit

--- src/molecules.ts
+++ src/molecules.ts
@@ -1,8 +1,64 @@
+// www.codewars.com/kata/52f831fa9d332c6591000511/train/typescript
+
 import { assertEqual } from "./helpers/assertEqual";
 
+// Handle different brackets in the formula ✅
+// Handle nested brackets in the formula ❌
+// One way of dealing it is too index braces, somethig like that (³
+// but this is kind of a stupid idea, as it is easier, just to find the most centered brackets and work from there
+
+// Decipher which level of brackets is more nested ❌
+// Disect atoms in the formula ❌
+// Multiply the count of atoms in the formula ❌
+
+function replaceBrackets(formula: string): string {
+  formula = formula.replace(/{/g, "(");
+  formula = formula.replace(/}/g, ")");
+
+  formula = formula.replace(/\[/g, "(");
+  formula = formula.replace(/\]/g, ")");
+
+  return formula;
+}
+
+// TODO
+// Find the most nested brackets
+// Parse
+// Work on outer bit
+// Parse
+// Return final result
+
+// I can definitely easy find the first closing bracket, and then find the first opening bracket before that
+
+// let's replace  inner brackets with {} to mark the inner zone
+// evaluate the expression, bu adding the result to the hashmap
+// and then cutting this inner part off
+// wait, I can't cut it off, as it might be affected by the outer brackets
+// so let's firstly evaluate, the inner brackets and then replace them with the result
+
+function hasBrackets(formula: string): boolean {
+  return formula.includes("(");
+}
+
 export function parseMolecule(formula: string): Record<string, number> {
-  const groups = formula.match(/[A-Z][a-z]*\d*|\([^\)]+\)\d*/g);
-  console.log("groups:", groups);
+  formula = replaceBrackets(formula);
+
+  console.log({ formula });
+
+  if (hasBrackets(formula)) {
+    formula = formula.replace(")", "}"); // replace the first closing bracket with }
+
+    let closingBracketIndex = formula.indexOf("}");
+    let openingBracketIndex = formula.lastIndexOf("(", closingBracketIndex);
+
+    // Keep debugging tomorrow
+    console.log({
+      closingBracketIndex,
+      openingBracketIndex,
+      s1: formula[closingBracketIndex],
+      s2: [openingBracketIndex],
+    });
+  }
 
   return {};
 }
@@ -33,24 +89,3 @@ assertEqual({
     S: 4,
   },
 });
-
-// TODO:
-// parser
-// Collect atoms with their appropriate group
-// Handle brackets
-// Multiple the count
-
-// I can hack it with regex, or some regex solution, but I want to do it properly
-
-// This is really kyo5 even though some say it's kyo4
-// Cuz I already worked with brackets for the math kata, so I do have an experience, and it should be easier for me, though that time, I kind of hacked it
-
-// Maybe drill the inner group, and just multiply the count, and then merge it with the outer group
-// Though, I need a logic to select the atom, as it can be a single letter or a double letter
-
-// It would be really fun to solve, it without regex
-
-// Basically this problem boils down, to two subproblems
-// Disect atoms
-// Handle parentheses
-// Handle nested parentheses (this one is really hard to solve without)
