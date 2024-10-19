a3f5a93b41b1891df02e77a011a0dcc05c5a64d0
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-10-19 16:44:03 +0300

    Uncomment frequency tests

--- test/solution.test.ts
+++ test/solution.test.ts
@@ -639,118 +639,107 @@ describe("SQL tests", function () {
     ).to.deep.equal([1, 2, 5, 6, 7, 8, 9]);
   });
 
-  // it("Frequency tests", () => {
-  //   var persons = [
-  //     ["Peter", 3],
-  //     ["Anna", 4],
-  //     ["Peter", 7],
-  //     ["Michael", 10],
-  //   ];
-
-  //   function nameGrouping(person: any) {
-  //     return person[0];
-  //   }
-
-  //   function sumValues(value: any) {
-  //     return [
-  //       value[0],
-  //       value[1].reduce(function (result: any, person: any) {
-  //         return result + person[1];
-  //       }, 0),
-  //     ];
-  //   }
-
-  //   function naturalCompare(value1: any, value2: any) {
-  //     if (value1 < value2) {
-  //       return -1;
-  //     } else if (value1 > value2) {
-  //       return 1;
-  //     } else {
-  //       return 0;
-  //     }
-  //   }
-
-  //   // SELECT name, sum(value) FROM persons ORDER BY naturalCompare GROUP BY nameGrouping
-  //   expect(
-  //     query()
-  //       .select(sumValues)
-  //       .from(persons)
-  //       .orderBy(naturalCompare)
-  //       .groupBy(nameGrouping)
-  //       .execute()
-  //   ).to.deep.equal([
-  //     ["Anna", 4],
-  //     ["Michael", 10],
-  //     ["Peter", 10],
-  //   ]);
-
-  //   var numbers = [1, 2, 1, 3, 5, 6, 1, 2, 5, 6];
-
-  //   function id(value: any) {
-  //     return value;
-  //   }
-
-  //   function frequency(group: any) {
-  //     return {
-  //       value: group[0],
-  //       frequency: group[1].length,
-  //     };
-  //   }
-
-  //   // SELECT number, count(number) FROM numbers GROUP BY number
-  //   expect(
-  //     query().select(frequency).from(numbers).groupBy(id).execute()
-  //   ).to.deep.equal([
-  //     {
-  //       value: 1,
-  //       frequency: 3,
-  //     },
-  //     {
-  //       value: 2,
-  //       frequency: 2,
-  //     },
-  //     {
-  //       value: 3,
-  //       frequency: 1,
-  //     },
-  //     {
-  //       value: 5,
-  //       frequency: 2,
-  //     },
-  //     {
-  //       value: 6,
-  //       frequency: 2,
-  //     },
-  //   ]);
-
-  //   function greatThan1(group: any) {
-  //     return group[1].length > 1;
-  //   }
-
-  //   function isPair(group: any) {
-  //     return group[0] % 2 === 0;
-  //   }
-
-  //   // SELECT number, count(number) FROM numbers GROUP BY number HAVING count(number) > 1 AND isPair(number)
-  //   expect(
-  //     query()
-  //       .select(frequency)
-  //       .from(numbers)
-  //       .groupBy(id)
-  //       .having(greatThan1)
-  //       .having(isPair)
-  //       .execute()
-  //   ).to.deep.equal([
-  //     {
-  //       value: 2,
-  //       frequency: 2,
-  //     },
-  //     {
-  //       value: 6,
-  //       frequency: 2,
-  //     },
-  //   ]);
-  // });
+  it("Frequency tests", () => {
+    var persons = [
+      ["Peter", 3],
+      ["Anna", 4],
+      ["Peter", 7],
+      ["Michael", 10],
+    ];
+    function nameGrouping(person: any) {
+      return person[0];
+    }
+    function sumValues(value: any) {
+      return [
+        value[0],
+        value[1].reduce(function (result: any, person: any) {
+          return result + person[1];
+        }, 0),
+      ];
+    }
+    function naturalCompare(value1: any, value2: any) {
+      if (value1 < value2) {
+        return -1;
+      } else if (value1 > value2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+    // SELECT name, sum(value) FROM persons ORDER BY naturalCompare GROUP BY nameGrouping
+    expect(
+      query()
+        .select(sumValues)
+        .from(persons)
+        .orderBy(naturalCompare)
+        .groupBy(nameGrouping)
+        .execute()
+    ).to.deep.equal([
+      ["Anna", 4],
+      ["Michael", 10],
+      ["Peter", 10],
+    ]);
+    var numbers = [1, 2, 1, 3, 5, 6, 1, 2, 5, 6];
+    function id(value: any) {
+      return value;
+    }
+    function frequency(group: any) {
+      return {
+        value: group[0],
+        frequency: group[1].length,
+      };
+    }
+    // SELECT number, count(number) FROM numbers GROUP BY number
+    expect(
+      query().select(frequency).from(numbers).groupBy(id).execute()
+    ).to.deep.equal([
+      {
+        value: 1,
+        frequency: 3,
+      },
+      {
+        value: 2,
+        frequency: 2,
+      },
+      {
+        value: 3,
+        frequency: 1,
+      },
+      {
+        value: 5,
+        frequency: 2,
+      },
+      {
+        value: 6,
+        frequency: 2,
+      },
+    ]);
+    function greatThan1(group: any) {
+      return group[1].length > 1;
+    }
+    function isPair(group: any) {
+      return group[0] % 2 === 0;
+    }
+    // SELECT number, count(number) FROM numbers GROUP BY number HAVING count(number) > 1 AND isPair(number)
+    expect(
+      query()
+        .select(frequency)
+        .from(numbers)
+        .groupBy(id)
+        .having(greatThan1)
+        .having(isPair)
+        .execute()
+    ).to.deep.equal([
+      {
+        value: 2,
+        frequency: 2,
+      },
+      {
+        value: 6,
+        frequency: 2,
+      },
+    ]);
+  });
 
   // it("Join tests", () => {
   //   var teachers = [
